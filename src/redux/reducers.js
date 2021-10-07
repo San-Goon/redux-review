6e8c0fd7936b49758b75dc0d9339635f79a6b25a
Kyle <tigerlove95@naver.com>
2021-10-07 13:41:04 +0900

    211007

---
+++ src/redux/reducers.js
@@ -0,0 +1,11 @@
+import { ADD_TODO } from "./actions";
+
+const initialState = [];
+
+export function todoApp(previousState = initialState, action) {
+  if (action.type === ADD_TODO) {
+    return [...previousState, action.todo];
+  }
+
+  return previousState;
+}
